7a53a224db267d27a82dfc2ad00c81f94aba32a1
konaten <konatenhamed@gmail.com>
2022-12-13 18:51:54 +0000

    save du soir

--- cemedo-app/921.389c06d2ad01bdb9.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkcemedoApp=self.webpackChunkcemedoApp||[]).push([[921],{32761:(d,l,_)=>{_.d(l,{_:()=>c});var p=_(69808),m=_(45082),o=_(5e3);let c=(()=>{class e{}return e.\u0275fac=function(a){return new(a||e)},e.\u0275mod=o.oAB({type:e}),e.\u0275inj=o.cJS({providers:[m.m],imports:[[p.ez]]}),e})()}}]);
